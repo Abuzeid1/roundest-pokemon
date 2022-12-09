cf86cbbab2f9b66ac3c7e8dcf9d1177150f6ea57
Abuzeid <abuzeid1999@gmail.com>
2022-12-09 13:56:47 +0200

    fixed border box around image while loading in safari 15

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -12,10 +12,10 @@ const ResultPage: NextPage<InferGetServerSidePropsType<typeof getStaticProps>> =
             {pokemonScores.map(({ id, _count, spriteUrl, score, name }, index) => {
 
                 return (
-                    <li key={id} className="grid-cols-[1fr_1fr_.5fr] grid sm:grid-cols-[1.5fr_1fr_.5fr_.5fr_.5fr]  sm:gap-2 items-center text-left w-full max-w-2xl border sm:p-2 sm:px-7 mx-auto sm:text-2xl max-sm:text-sm relative">
+                    <li key={id} className="grid-cols-[1fr_1fr_.5fr] grid sm:grid-cols-[1fr_1fr_.5fr_.5fr_.5fr]  sm:gap-2 items-center text-left w-full max-w-2xl border sm:p-2 sm:px-7 mx-auto sm:text-2xl max-sm:text-sm relative">
 
                         <span className="absolute top-0 left-0 bg-gray-200 text-gray-700 p-2 max-sm:p-1 rounded-br-lg">{index + 1}</span>
-                        <Image src={spriteUrl} placeholder="empty" width="200" height="200" alt="" />
+                        <Image className="w-full h-auto max-h-full" src={spriteUrl} width={200} height={200} alt={name} />
                         <h2 className="capitalize"> {name}</h2>
 
                         <div className="sm:block hidden">{_count.VoteFor | 0}</div>
