6bce4bba96967a8ec3869f8e8faf4a80ec241803
Abuzeid <abuzeid1999@gmail.com>
2023-04-06 00:37:49 +0200

    using context to intiate prisma client in tRpc router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -3,35 +3,38 @@ import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 import { exampleRouter } from "./example";
 
-import {PokemonClient } from "pokenode-ts"
-import { prisma } from "../../db/client";
-import { TRPCError } from "@trpc/server";
+import { PokemonClient } from "pokenode-ts";
+
 import { getOptionsForVote } from "../../../utils/getRandomPokemon";
 const api = new PokemonClient();
 
 export const appRouter = router({
   example: exampleRouter,
-  pokemonPair: publicProcedure.query(async () => {
-    const [id1, id2] = getOptionsForVote()
-    const pokemonPair = await prisma.pokemon.findMany({where:{OR: [{id: id1}, {id: id2}]}})
-    if(!pokemonPair) throw new Error("lol doesn't exist")
-    
-    const [firstPokemon, secondPokemon] = pokemonPair
+  pokemonPair: publicProcedure.query(async ({ ctx }) => {
+    const [id1, id2] = getOptionsForVote();
 
-    return {firstPokemon, secondPokemon}
+    const pokemonPair = await ctx.prisma.pokemon.findMany({
+      where: { OR: [{ id: id1 }, { id: id2 }] },
+    });
+    if (!pokemonPair) throw new Error("lol doesn't exist");
+
+    const [firstPokemon, secondPokemon] = pokemonPair;
+
+    return { firstPokemon, secondPokemon };
   }),
-  
-  pokemonVoteUpdate: publicProcedure.input(z.object({votedFor: z.number(), votedAgainst: z.number()})).mutation(async ({input}) => {
-    const voteInDb = await prisma.vote.create({
-      data: {
-        votedForId: input.votedFor,
-        votedAgainstId: input.votedAgainst,
-      }
-    })
 
+  pokemonVoteUpdate: publicProcedure
+    .input(z.object({ votedFor: z.number(), votedAgainst: z.number() }))
+    .mutation(async ({ input, ctx }) => {
+      const voteInDb = await ctx.prisma.vote.create({
+        data: {
+          votedForId: input.votedFor,
+          votedAgainstId: input.votedAgainst,
+        },
+      });
 
-    return {"success": true, vote: voteInDb}
-  })
+      return { success: true, vote: voteInDb };
+    }),
 });
 
 // export type definition of API
