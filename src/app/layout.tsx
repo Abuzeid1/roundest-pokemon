87e1c22f87c5513b14e63ecffa1f7801017b414b
Abuzeid <abuzeid1999@gmail.com>
2023-09-20 20:07:49 +0300

    removing extra files and code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body className="text-red-200; bg-gray-800 text-2xl">
-        <main className=" flex min-h-[40rem] w-screen flex-col items-center text-gray-50 sm:h-screen  ">
+        <main className="flex min-h-screen w-screen flex-col items-center text-gray-50">
           {children}
         </main>
       </body>
